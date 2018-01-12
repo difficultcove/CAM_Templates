8ec9466f7c584e17bf540c46bb1a6acb85aace3f
difficultcove <tonyfrench@tf-mac-client.home>
2018-01-12 15:33:28 +0000

    1.6

--- VMware/terraform/hcl/Tririga/Tririga.tf
+++ VMware/terraform/hcl/Tririga/Tririga.tf
@@ -45,6 +45,11 @@ variable "vcpu" {
   default     = 1
 }
 
+variable "rootdisksize" {
+  description = "The Size of the root disk"
+  default     = 16
+}
+
 variable "memory" {
   description = "Memory for Virtual Machine in GBs"
   default     = 1
