ee641d98a7ee00e3d3a15178769d5aae2afcbe5e
difficultcove <tonyfrench@tf-mac-client.local>
2018-01-22 17:56:34 +0000

    2.2.9

--- VMware/terraform/hcl/singleVMRootDisk/main.tf
+++ VMware/terraform/hcl/singleVMRootDisk/main.tf
@@ -121,6 +121,7 @@ resource "vsphere_virtual_machine" "vm_1" {
   memory            = "${var.memory}"
   resource_pool_id  = "${data.vsphere_resource_pool.pool.id}"
   datastore_id      = "${data.vsphere_datastore.datastore.id}"
+  guest_id          = "${data.vsphere_virtual_machine.template.guest_id}"
 
   network_interface {
       network_id = "${data.vsphere_network.network.id}"
