2b0d2aa183b81b31d36d22ef7a78f441771e16fd
difficultcove <tonyfrench@tf-mac-client.local>
2018-01-26 16:55:27 +0000

    2.6.1.1

--- VMware/terraform/hcl/SingleVM-v1.1-Linux/main.tf
+++ VMware/terraform/hcl/SingleVM-v1.1-Linux/main.tf
@@ -130,6 +130,10 @@ resource "vsphere_virtual_machine" "vm_1" {
   resource_pool_id  = "${data.vsphere_resource_pool.pool.id}"
   datastore_id      = "${data.vsphere_datastore.datastore.id}"
   guest_id          = "${data.vsphere_virtual_machine.template.guest_id}"
+  num_cores_per_socket = 1
+  cpu_hot_add_enabled  = true
+  cpu_hot_remove_enabled = true
+  memory_hot_add_enabled = true
 
   network_interface {
       network_id    = "${data.vsphere_network.network.id}"
