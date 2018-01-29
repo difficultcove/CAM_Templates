a79d7f8b0d7440182484736e64530c7c12fa23a3
difficultcove <tonyfrench@tf-mac-client.local>
2018-01-29 14:32:33 +0000

    2.6.1.12

--- VMware/terraform/hcl/SingleVM-v1.1-Windows/main.tf
+++ VMware/terraform/hcl/SingleVM-v1.1-Windows/main.tf
@@ -110,7 +110,7 @@ data "vsphere_virtual_machine" "template" {
 
 ############### Optional settings in provider ##########
 provider "vsphere" {
-    version 							= "~> 1.3"
+    version 							= "1.1"
     allow_unverified_ssl 	= "${var.allow_selfsigned_cert}"
 }
 
