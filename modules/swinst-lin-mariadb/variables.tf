fe193a76c7ee3a5e8b449f5de3214e9fd05c91ba
difficultcove <tonyfrench@tf-mac-client.connect>
2019-01-24 17:09:43 +0000

    Update variables.tf

--- modules/swinst-lin-mariadb/variables.tf
+++ modules/swinst-lin-mariadb/variables.tf
@@ -1,6 +1,14 @@
 ##################### Variables ###############################
 
 
+variable "ssh_user" {
+  description = "The user for ssh connection, which is default in template"
+  default     = "root"
+}
+
+variable "ssh_user_private_key" {
+  description = "The user private key for ssh connection, which is default in template"
+}
 
 variable "dependsOn" {
   description = "depends On variable for VM creation"
