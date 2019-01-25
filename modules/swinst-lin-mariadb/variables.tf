cd3a75c83e8baf940f731918a039347f58b61a1e
difficultcove <tonyfrench@tf-mac-client.connect>
2019-01-25 09:18:35 +0000

    Update variables.tf

--- modules/swinst-lin-mariadb/variables.tf
+++ modules/swinst-lin-mariadb/variables.tf
@@ -10,6 +10,14 @@ variable "ssh_user_private_key" {
   description = "The user private key for ssh connection, which is default in template"
 }
 
+#Target VM IP address
+variable "ipv4_subnet" {
+  type = "string"
+}
+variable "ipv4_subnet_index" {
+  type = "string"
+}
+
 variable "dependsOn" {
   description = "depends On variable for VM creation"
   default     = true
