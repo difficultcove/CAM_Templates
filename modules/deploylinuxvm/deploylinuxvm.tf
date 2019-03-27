d439ba0c003f704df64173d71bdd32c328204a4a
difficultcove <tonyfrench@tf-mac-client.connect>
2019-03-27 18:26:33 +0000

    Update deploylinuxvm.tf

--- modules/deploylinuxvm/deploylinuxvm.tf
+++ modules/deploylinuxvm/deploylinuxvm.tf
@@ -63,6 +63,7 @@ resource "vsphere_virtual_machine" "vm_1" {
   disk {
     label              = "${var.name}-3.vmdk"
     size              = "${var.rootdisksize}"
+    datastore_id      = "${data.vsphere_datastore.datastore.id}"
     attach            = true
     path              = "ESX6-volume-01/${var.name}-3.vmdk"
     unit_number = 3
